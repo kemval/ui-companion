54c172cc0a3c687383f8dbb3a527ae1ed7ca9103
kemval <kemval@icloud.com>
2023-11-22 15:20:32 -0600

    category filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,16 @@
-import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb"
+import { Categories } from "@/components/categories"
+import { SearchInput } from "@/components/search-input"
 
-const RootPage = () => {
-    return ( 
+
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany();
+    return(
         <div className="h-full p-4 space-y-2">
             <SearchInput/>
+            <Categories data={categories}/>
+
         </div>
-     );
+    );
 }
- 
-export default RootPage;
+export default RootPage
